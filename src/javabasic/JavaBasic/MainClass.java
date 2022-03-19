54c9158fb52b95b7ada1e8a98993991c1d6148cb
ngoxuanchien <ngoxuanchien9a@gmail.com>
2022-03-20 00:30:51 +0700

    Init Project

--- src/javabasic/JavaBasic/MainClass.java
+++ src/javabasic/JavaBasic/MainClass.java
@@ -4,6 +4,8 @@
  */
 package javabasic.JavaBasic;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ngoxu
@@ -15,7 +17,25 @@ public class MainClass {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello");
+        String fullname;
+        int age;
+        
+        Scanner scan = new Scanner(System.in);
+        // Nhap du lieu
+        System.out.println("Nhap ten: ");
+        fullname = scan.nextLine();
+        System.out.println("Nhap tuoi");
+        age = scan.nextInt();
+        if (age < 6) {
+            System.out.println("Hoc mau giao");
+        } else if (age <= 11) {
+            System.out.println("Hoc tieu hoc");
+        } else if (age <= 15) {
+            System.out.println("Hoc trung hoc");
+        } else if (age <= 18) {
+            System.out.println("Hoc pho thong");
+        }
+        
     }
     
 }
